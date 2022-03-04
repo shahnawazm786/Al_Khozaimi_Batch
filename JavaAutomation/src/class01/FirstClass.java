7bf18c027e977fa3856495bc83837fd2cfef9066
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-03-04 21:56:50 +0530

    First Class

---
+++ JavaAutomation/src/class01/FirstClass.java
@@ -0,0 +1,50 @@
+package class01;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// println() -> print line 
+		// message
+		// value
+		// resultant show
+		// combination (Message + value | Message + result)
+		// Message combine "+" 
+		// Message + value
+		// Message + resultant
+		// Message ""
+		System.out.println("Hello!! World");
+		
+		System.out.println(100+300);
+		
+		System.out.println(400*3);
+		
+		// variable 100 +110 - int/long
+		// 123.50 - float /double
+		// 'Y', 'M', 'F' - char
+		// T or F - boolean
+		// word - sentence - pragraph 
+		// String object
+		// byte 
+		
+		// datatype <variable_name>;
+		int x=100;
+		int y=200;
+		int a=100, b=200;
+		System.out.println("X =>" + x);
+		System.out.println("Addition of X =>"+x +" and Y=>"+y+" ==>"+(x+y));
+		float z=10.99f;
+		float res=x+z;
+		System.out.println(res);
+		x=(int)z+y;
+		System.out.println(x);
+		double d=3456.999d;
+		boolean flag=true;
+		char gender='M';
+		System.out.println("Double data type =>"+d);
+		System.out.println("Boolean data type =>"+flag);
+		System.out.println("char data type =>"+gender);
+		
+	}
+
+}
