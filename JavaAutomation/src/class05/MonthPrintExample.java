6b01160e9f68c2afee3ef0a3c68389cd3cbae1da
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-03-11 09:33:29 +0530

    month

---
+++ JavaAutomation/src/class05/MonthPrintExample.java
@@ -0,0 +1,18 @@
+package class05;
+
+import java.util.Scanner;
+
+public class MonthPrintExample {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+					Scanner s=new Scanner(System.in);
+					System.out.println("Enter day");
+					int month=s.nextInt();
+					//printDay(d);
+					printMonthUsingSwitchCase(month);	
+	}
+	static void printMonthUsingSwitchCase(int month) {
+		
+	}
+	
+}
