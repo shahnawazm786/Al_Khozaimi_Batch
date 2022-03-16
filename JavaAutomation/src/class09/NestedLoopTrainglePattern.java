7f531e3a7e0d93dd870c05046746840f7576a8d1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-03-16 23:02:55 +0530

    pattern

--- JavaAutomation/src/class09/NestedLoopTrainglePattern.java
+++ JavaAutomation/src/class09/NestedLoopTrainglePattern.java
@@ -7,8 +7,8 @@ public class NestedLoopTrainglePattern {
 		//startPrint();
 		//numberPrint();
 		//numberPrint1();
-		numberPrint2();
-		
+		//numberPrint2();
+		numberPrint3();
 	}
 	static void startPrint() {
 		int x=1;
@@ -78,28 +78,32 @@ public class NestedLoopTrainglePattern {
 		}
 	}
 
-	static void numberPrint2() {
+	static void numberPrint3() {
 		int x=1;
 		int y;
-		while(x<=4) {
+		int z;
+		int a;
+		a=7;
+		while(x<=7) {
 			y=1;
-			while(y<=x) {
+			while(y<=x & x<=4) {
 				System.out.print(y +" ");
 				y++;
 			}
+		
+			System.out.println();
+			z=1;
+			while(z<=(a-x) & x>=4) {
+				//System.out.println(x-a);
+				//System.out.println(a);
+				System.out.print(z+" ");
+				z++;
+			}
+			
 			x++;
-			System.out.println();
 		}
-		//System.out.println(x);
-		while(x-2>=1) {
-			y=1;
-			while(y<=x-2) {
-				System.out.print(y +" ");
-				y++;
-			}
-			x--;
-			System.out.println();
-		}
+		
+		
 	}
 	
 }
