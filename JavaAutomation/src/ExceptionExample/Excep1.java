1c5aa80dd9e0d08924ea23703af25631132526fe
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-05-25 23:02:40 +0530

    exceptions

---
+++ JavaAutomation/src/ExceptionExample/Excep1.java
@@ -0,0 +1,26 @@
+package ExceptionExample;
+
+import java.util.Scanner;
+
+public class Excep1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number ->");
+		a=sc.nextInt();
+		System.out.println("Enter number ->");
+		b=sc.nextInt();
+		System.out.println("Statement 1");
+		try {
+		System.out.println("Statement 2 -> "+ a/b);
+		}catch(ArithmeticException ae) {
+			System.out.println("B =" + b);
+			System.out.println("Please user other value");
+		}
+		System.out.println("Statement 3");
+		System.out.println("Statement 4");
+	}
+
+}
